7505d29c912b253abc5151f0fb5ba24628b76374
leshiy77 <rifbenya@gmail.com>
2022-05-17 12:16:33 +0300

    feat: add 1 part task

---
+++ 01-read-file/index.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const path = require('path');
+
+const pathToFile = path.join(__dirname, 'text.txt');
+const stream = fs.createReadStream(pathToFile, 'utf-8');
+
+stream.on('error', (err) => {
+  if (err.code === 'ENOENT') {
+    console.error('no such file in directory');
+  } else {
+    throw err;
+  }
+});
+
+stream.on('data', (chunk) => {
+    console.log(chunk)
+})
