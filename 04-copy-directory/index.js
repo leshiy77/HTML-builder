74b2d94e596bc62bbc0d663ffaff08ab0f66424b
leshiy77 <rifbenya@gmail.com>
2022-05-21 10:35:30 +0300

    fix: task4 renew solution

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -5,6 +5,20 @@ const fs = require('fs');
 const pathToDir = path.join(__dirname, 'files');
 const pathToCopyDir = path.join(__dirname, 'files-copy');
 
+fs.readdir(pathToCopyDir, (err, files) => {
+  if (err) {
+    return;
+  } else {
+    files.forEach(file => {
+      fs.unlink(path.join(pathToCopyDir, file), (err) => {
+        if (err) {
+          console.log(err);
+        }
+      })
+    });
+  }  
+});
+
 async function copyDir (dir, copyDir) {
   fsPromises.mkdir(copyDir, { recursive: true });
   try {
